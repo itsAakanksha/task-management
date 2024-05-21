b3b08cdff0f09317ce2de7b836efd0dda4255df0
Aakanksha <82.25aakanksha@gmail.com>
2024-05-22 02:44:53 +0530

    changes in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import App from './App';
 import { store } from './App/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <Provider store={store}>
     <App />
   </Provider>
